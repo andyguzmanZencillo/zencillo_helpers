4ebe6b49fc86d2359ae5f5c08f8bc1b34acd8477
andyguzmanZencillo <166416095+andyguzmanZencillo@users.noreply.github.com>
2026-06-25 23:31:33 -0400

    add qr payment types enum

--- lib/src/enums/tipo_dataphono_enum.dart
+++ lib/src/enums/tipo_dataphono_enum.dart
@@ -32,3 +32,57 @@ enum TipoDataphono {
     );
   }
 }
+
+enum SettingsPrinter {
+  none('Ninguno'),
+  remote('IMPRESIÓN REMOTA'),
+  nexgo('NEXGO'),
+  sunmi('SUNMI'),
+  iceelectronics('ICE ELECTRONICS'),
+  credibanco('CREDIBANCO'),
+  ciontek('CIONTEK'),
+  urovo('UROVO'),
+  imin('IMIN'),
+  twoconnect('2CONNECT'),
+
+  medianetPos('MEDIANET POS'),
+
+  cardnetPos('CARDNET POS'),
+  aon('AON');
+
+  const SettingsPrinter(this.text);
+
+  final String text;
+}
+
+enum SettingsQR {
+  none('Ninguno'),
+  camara('CAMARA'),
+  lectorqrcr('LECTOR/QR CR'),
+  lectorqrnexgo('LECTOR/QR NEXGO'),
+  lectorqrsunmy('LECTOR/QR SUNMI'),
+  lectorqrurovo('LECTOR/QR UROVO');
+
+  const SettingsQR(this.text);
+
+  final String text;
+}
+
+enum SettingsNFC {
+  none('Ninguno'),
+  nfczencillo('NFC Zencillo'),
+  nfcnexgo('NFC NEXGO'),
+  nfccr('NFC CR');
+
+  const SettingsNFC(this.text);
+
+  final String text;
+}
+
+enum SettingsRFID {
+  none('NO APLICA RFID'),
+  urovorfidepc('UROVO RFID/EPC');
+
+  const SettingsRFID(this.text);
+  final String text;
+}
